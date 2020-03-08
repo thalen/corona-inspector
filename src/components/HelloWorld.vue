1abb8dfe883caf70d35763c30e25b9c4e8ffe3d4
Olle Thalén <olle@thalen-solutions.com>
2020-03-08 18:05:50 +0100

    first search

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,48 @@
 <template>
-  <div class="hello">
-    hej hjej
-  </div>
+  <v-container id="input-usage"
+fluid>
+    <v-row>
+      <v-col cols="8">
+        <label>Landskod</label>
+        <input v-model="form.country"
+style="margin-left:5px;border: 1px solid;border-radius: 2px"
+/>
+      </v-col>
+      <v-col cols="4">
+        <v-btn small
+v-on:click="search">
+          Sök
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row v-if="latest">
+      <v-col cols="12">
+Senaste: {{ latest }}
+</v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import * as ax from 'axios';
+
+const axios = ax.default;
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      form: {
+        country: ''
+      },
+      latest: null
+    };
+  },
+  methods: {
+    search() {
+      axios.get(`/api/confirmed/${this.form.country}`).then(({ data }) => {
+        this.latest = data.country.latest;
+      });
+    }
   }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  transpileDependencies: ['vuetify']
+  transpileDependencies: ['vuetify'],
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        secure: false
+      }
+    }
+  }
 };
